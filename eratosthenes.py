9d3d0897cd0f3b1bd3a3ddb111e5768b47caabda
TheSpeedyM <muhammedibneali@gmail.com>
2026-03-23 16:39:10 +0600

    Add files via upload

---
+++ eratosthenes.py
@@ -0,0 +1,16 @@
+def sieveoferatosthenes(n):
+    prime = [True for i in range(n + 1)]
+    p = 2
+    while (p * p <= n):
+        if (prime[p] == True):
+            for i in range(p * p, n + 1, p):
+                prime[i] = False
+        p += 1
+    for p in range(2, n):
+        if prime[p]:
+            print(p)
+
+
+num = int(input("Enter the number: "))
+print("The prime numbers smaller than or equal to", num, "are:")
+sieveoferatosthenes(num)
